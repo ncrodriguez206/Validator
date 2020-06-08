b7a4aea5590f58988f6932bd12eea8c27a3a0a61
ApuntaDePuntos <apuntadepuntos@gmail.com>
2020-06-08 17:55:58 -0500

    Co-authored-by: loregunner <dahara59@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+var getData = function (){
+    var nombre = document.getElementById("nombres").value;
+    
+    var creditCardNumber = document.getElementById("creditCardNumber").value;
+    console.log(nombre,creditCardNumber);
+  
+    localStorage.setItem("nombre",nombre);
+    localStorage.setItem("creditCardNumber",creditCardNumber); 
+    
+    window.location.href = 'indexP4.html'} 
---
+++ src/indexP1.js
@@ -0,0 +1,2 @@
+function categoriaUno() {window-location.href= "indexP2.html";}
+function categoriaDos() {window-location.href= "indexP3.html";}
