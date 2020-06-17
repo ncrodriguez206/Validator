5c892cfcb9f462cb188ca2c6726872a6a18c9f4c
ApuntaDePuntos <apuntadepuntos@gmail.com>
2020-06-17 12:58:11 -0500

    Co-authored-by: loregunner <dahara59@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
-//import validator from './validator.js';
+import validator from './validator.js';
 
-//console.log(validator);
+console.log(validator);
 
-function getData() {
+function alias(){
 
-    var nombre = document.getElementById("nombres").value;
-    var creditCardNumber1= document.getElementById("creditCardNumber").value;
+  var creditCardNumber1 = document.getElementById("creditCardNumber").value;
+  validator.isvalid (creditCardNumber1)
+  validator.maskify (creditCardNumber1)
+  //  window.location.href = 'indexP4.html' 
 
-    // console.log(getData)
-    console.log(nombre, creditCardNumber1);
-    localStorage.setItem("nombre", nombre);
-    localStorage.setItem("creditCardNumber1", creditCardNumber1);
+};
 
-    const aNumeroTarjeta= Array.from(creditCardNumber1);
-    console.log(aNumeroTarjeta)
-
-    const aInvertido = aNumeroTarjeta.reverse();
-    console.log(aNumeroTarjeta)
+  
 
 
-    //  window.location.href = 'indexP4.html' 
-  }
-
-  const boton = document.getElementById("BotonTarjeta");
-  boton.addEventListener("click", getData); 
+const boton = document.getElementById("BotonTarjeta");
+boton.addEventListener("click", alias); 
--- src/validator.js
+++ src/validator.js
@@ -1,15 +1,50 @@
-// Traer numero tarjeta 
-const numeroTarjeta= document.getElementById ("creditCardNumber").Value;
-// Volver sting en array 
-const aNumeroTarjeta= parseInt (Array.from("numeroTarjeta"));
-// Invertir el array 
-const aInvertido= aNumeroTarjeta.reverse(); 
-console.log (aInvertido)
 
-/*const validator = {
 
-  Isvalid: (creditCardNumber) = 
+const validator = {
+
+    isvalid: function getData() {
+      
+      var creditCardNumber1 = document.getElementById("creditCardNumber").value;
+
+      const aNumeroTarjeta = Array.from(creditCardNumber1);
+      console.log(aNumeroTarjeta)
+
+      const aInvertido = aNumeroTarjeta.reverse();
+      console.log(aInvertido)
+  
+     aInvertido.forEach (function (element, index) {
+      let nueva = index + 1;
+      if (nueva % 2 == 0) { element = element * 2 };
+  
+      if (element > 9) { element = element - 9 }
+  
+      aInvertido[index] = element;
+  
+      console.log(aInvertido);
+  
+      });
+  
+    console.log(aInvertido);
+    var suma = 0;
+  
+    for (var i = 0; i < aInvertido.length; i++) {
+      suma = suma + parseInt(aInvertido[i]);
+  
+    }
+  
+    console.log(suma);
+  
+    var division = suma % 10
+    console.log(division);
+  
+    if (division === 0) { return true } else { return false }
+  
+  },
+  
+
+    maskify: (aNumeroTarjeta)=> aNumeroTarjeta.slice(0, -4).fill('#') + aNumeroTarjeta.slice(-4)
+
+
 }
 
-
-export default validator;*/
+export default validator;
